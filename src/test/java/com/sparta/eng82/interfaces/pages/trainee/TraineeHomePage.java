83f9e6f2daad24f40ea52e29661a0f206d4dbd34
jackingham <jackingham1998@gmail.com>
2021-05-17 17:06:11 +0100

    Populated interface

--- src/test/java/com/sparta/eng82/interfaces/pages/trainee/TraineeHomePage.java
+++ src/test/java/com/sparta/eng82/interfaces/pages/trainee/TraineeHomePage.java
@@ -3,4 +3,16 @@ package com.sparta.eng82.interfaces.pages.trainee;
 import com.sparta.eng82.interfaces.Page;
 
 public interface TraineeHomePage extends Page {
+
+    TraineeFeedbackFormPage clickCurrentWeek();
+    TraineeFeedbackFormPage clickFeedbackFormForWee(int week);
+    //TODO Set up traffic light enum?
+    String getCurrentTrafficLight();
+    String getTrafficLightForWeek(int week);
+
+
+
+
+
+
 }
