d114a9b25c823dad4e6e837d9f7860c916799a57
Golam Choudhury <19grc97@gmail.com>
2021-05-17 17:08:13 +0100

    added some methods to the interface

--- src/test/java/com/sparta/eng82/interfaces/pages/trainer/TrainerHomePage.java
+++ src/test/java/com/sparta/eng82/interfaces/pages/trainer/TrainerHomePage.java
@@ -1,6 +1,15 @@
 package com.sparta.eng82.interfaces.pages.trainer;
 
 import com.sparta.eng82.interfaces.Page;
+import com.sparta.eng82.interfaces.pages.CompetenciesPage;
+import com.sparta.eng82.interfaces.pages.ProfilePage;
+import org.openqa.selenium.WebDriver;
 
 public interface TrainerHomePage extends Page {
+
+    public ManageGroupPageImpl manageGroupButton();
+
+    public TrainerFeedbackFormPageImpl selectTraineeName();
+
+    public boolean menuDropdownDisplays();
 }
