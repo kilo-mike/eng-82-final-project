0d6ce3907d4efc0360cd20847f6fc81d606809a2
jackingham <jackingham1998@gmail.com>
2021-05-18 12:19:32 +0100

    Created the WebDriverFactory class to return WebDriver objects when passed a browser type from the new WebDriverTypes enum. Added example tests to verify functionality of Chrome and Edge (remember that the relevant drivers are needed inside the resources folder to work.

---
+++ src/test/java/com/sparta/eng82/webdriver/WebDriverFactory.java
@@ -0,0 +1,31 @@
+package com.sparta.eng82.webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class WebDriverFactory {
+
+    static {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        System.setProperty("webdriver.edge.driver", "src/test/resources/msedgedriver.exe");
+    }
+
+
+    public WebDriver getWebDriver(WebDriverTypes driverType) {
+        WebDriver driver;
+        switch (driverType) {
+            case CHROME:
+                driver = new ChromeDriver();
+                break;
+            case EDGE:
+                driver = new EdgeDriver();
+                break;
+            default:
+                driver = null;
+                break;
+
+        }
+        return driver;
+    }
+}
