d61ab0d19df60a6e7d3a839dea3a26a29e5b6d14
alexsikorski <alexsikorski22@gmail.com>
2021-05-18 12:18:56 +0100

    fixed login interface and added method declarations into change password page

--- src/test/java/com/sparta/eng82/interfaces/pages/external/ChangePasswordPage.java
+++ src/test/java/com/sparta/eng82/interfaces/pages/external/ChangePasswordPage.java
@@ -3,4 +3,14 @@ package com.sparta.eng82.interfaces.pages.external;
 import com.sparta.eng82.interfaces.Page;
 
 public interface ChangePasswordPage extends Page {
+
+    ChangePasswordPage enterCurrentPassword(String password);
+
+    ChangePasswordPage enterNewPassword(String password);
+
+    ChangePasswordPage enterConfirmPassword(String password);
+
+    Page clickChange();
+
+    boolean changePasswordAttempt(String currentPassword, String newPassword, String confirmPassword);
 }
