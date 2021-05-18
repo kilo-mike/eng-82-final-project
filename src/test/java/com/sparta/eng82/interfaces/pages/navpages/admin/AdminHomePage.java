bfdcc93698cd781f87283dfb727cf15b405ee9c5
abdulwm97 <abdulwm09@gmail.com>
2021-05-18 12:19:48 +0100

    Added some possible tests to the AdminHomePage interface

--- src/test/java/com/sparta/eng82/interfaces/pages/navpages/admin/AdminHomePage.java
+++ src/test/java/com/sparta/eng82/interfaces/pages/navpages/admin/AdminHomePage.java
@@ -1,10 +1,13 @@
 package com.sparta.eng82.interfaces.pages.navpages.admin;
 
 import com.sparta.eng82.interfaces.pages.NavPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public interface AdminHomePage extends NavPage {
 
     AdminHomePage addTrainer();
 
-
+    boolean isTrainerAdded(String firstName, String lastName, String group);
+    String[] allTrainersAvailable();
 }
