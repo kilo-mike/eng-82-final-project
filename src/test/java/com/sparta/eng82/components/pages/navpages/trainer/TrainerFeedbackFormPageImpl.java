3a8fb07c78fb611c069a3a1383fc51c97588d668
alexsikorski <alexsikorski22@gmail.com>
2021-05-19 16:26:31 +0100

    deleted redundant class and added one test for login page

--- src/test/java/com/sparta/eng82/components/pages/navpages/trainer/TrainerFeedbackFormPageImpl.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.eng82.components.pages.navpages.trainer;
-
-import com.sparta.eng82.interfaces.pages.navpages.trainer.TrainerFeedbackFormPage;
-
-public class TrainerFeedbackFormPageImpl {
-
-}
--- src/test/java/com/sparta/eng82/tests/unit/other/LoginPageImplTests.java
+++ src/test/java/com/sparta/eng82/tests/unit/other/LoginPageImplTests.java
@@ -1,36 +1,52 @@
 package com.sparta.eng82.tests.unit.other;
 
 
+import com.sparta.eng82.components.pages.accesspages.LoginPageImpl;
 import com.sparta.eng82.components.webdriver.WebDriverFactory;
 import com.sparta.eng82.components.webdriver.WebDriverTypes;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-import java.util.EnumSet;
-import java.util.Set;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 public class LoginPageImplTests {
 
-    private static final EnumSet<WebDriverTypes> normalTypes = EnumSet.of(WebDriverTypes.CHROME, WebDriverTypes.EDGE);
-    static WebDriver driver;
-    static WebDriverFactory webDriverFactory;
+    private WebDriver driver;
+    private WebDriverFactory webDriverFactory;
+    private LoginPageImpl loginPage;
+
+    private final String adminPropertyUsername = "admin_username";
+    private final String adminPropertyPassword = "admin_password";
+
+    private static Properties properties;
 
     @BeforeAll
-    static void setupAll() {
+    static void setupAll(){
+        loadProperties(properties);
+    }
+
+    @BeforeEach
+    void setup() {
         webDriverFactory = new WebDriverFactory();
+        driver = webDriverFactory.getWebDriver(WebDriverTypes.CHROME);
+        loginPage = new LoginPageImpl(driver);
     }
 
-    private static Set<WebDriverTypes> normalTypesTest() {
-        return normalTypes;
+    @Test
+    @DisplayName("enterEmailTest")
+    void enterEmailTest() {
+        loginPage.enterEmail(adminPropertyUsername, properties);
+        Assertions.assertEquals(driver.findElement(new By.ById("username")).getAttribute("value"), properties.getProperty(adminPropertyUsername));
     }
 
-    @ParameterizedTest
-    @MethodSource("normalTypesTest")
-    @DisplayName("Test all normal types")
-    void testAllDriverTypes(WebDriverTypes webDriverType) {
-        driver = webDriverFactory.getWebDriver(webDriverType);
+    private static void loadProperties(Properties properties){
+        try {
+            properties.load(new FileReader("src/test/resources/login.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
