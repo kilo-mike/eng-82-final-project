78384c78f0c331f207dd7d95163fea52860da5a1
abdulwm97 <abdulwm09@gmail.com>
2021-05-20 16:29:43 +0100

    Added unit tests for AddTrainerPage. One test fails as one of the methods doesn't seem to work properly.

---
+++ src/test/java/com/sparta/eng82/tests/unit/admin/AddTainerPageTest.java
@@ -0,0 +1,89 @@
+package com.sparta.eng82.tests.unit.admin;
+
+import com.sparta.eng82.components.pages.accesspages.LoginPageImpl;
+import com.sparta.eng82.components.pages.navpages.admin.AdminHomePageImpl;
+import com.sparta.eng82.components.pages.navpages.admin.addpages.AddTrainerPageImpl;
+import com.sparta.eng82.components.webdriver.WebDriverFactory;
+import com.sparta.eng82.components.webdriver.WebDriverTypes;
+import com.sparta.eng82.tests.unit.utility.Utility;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Properties;
+
+public class AddTainerPageTest {
+    private static WebDriver driver;
+    private WebDriverFactory webDriverFactory;
+    private LoginPageImpl loginPage;
+    private AdminHomePageImpl adminHomePage;
+    private AddTrainerPageImpl addTrainerPage;
+    private final String adminPropertyUsername = "admin_username";
+    private final String adminPropertyPassword = "admin_password";
+    private final String adminPropertyName = "admin_name";
+    private static Properties properties;
+
+    private final String trainersFirstName = "Shelly";
+    private final String trainersLastName = "Brown";
+
+    @BeforeAll
+    static void setupAll() {
+        properties = new Properties();
+        Utility.loadProperties(properties);
+    }
+
+    @BeforeEach
+    void setup() {
+        webDriverFactory = new WebDriverFactory();
+        driver = webDriverFactory.getWebDriver(WebDriverTypes.CHROME_HEADLESS);
+        loginPage = new LoginPageImpl(driver);
+        adminHomePage = new AdminHomePageImpl(driver);
+    }
+
+    @Nested
+    class DoesTextShowUpInTextBox {
+        @Test
+        @DisplayName("Does text in first name show up?")
+        void doesTextInFirstNameShowUp() {
+            loginPage.enterEmail(driver, adminPropertyUsername, properties);
+            loginPage.enterPassword(driver, adminPropertyPassword, properties);
+            loginPage.login(driver, properties.getProperty(adminPropertyName));
+            adminHomePage.addTrainer().enterFirstName(trainersFirstName);
+           Assertions.assertEquals(trainersFirstName, driver.findElement(By.id("addtrainerFirstName")).getAttribute("value"));
+        }
+
+        @Test
+        @DisplayName("Does text in last name show up?")
+        void doesTextInLastNameShowUp() {
+            loginPage.enterEmail(driver, adminPropertyUsername, properties);
+            loginPage.enterPassword(driver, adminPropertyPassword, properties);
+            loginPage.login(driver, properties.getProperty(adminPropertyName));
+            adminHomePage.addTrainer().enterSecondName(trainersLastName);
+            Assertions.assertEquals(trainersLastName, driver.findElement(By.id("addtrainerLastName")).getAttribute("value"));
+        }
+    }
+
+    //TEST WILL FAIL!!!!! As a method isn't working properly!
+    @ParameterizedTest
+    @ValueSource(strings = {"hola", "No Group"})
+    @DisplayName("Can you select a group?")
+    void canYouSelectAGroup(String group) {
+        loginPage.enterEmail(driver, adminPropertyUsername, properties);
+        loginPage.enterPassword(driver, adminPropertyPassword, properties);
+        loginPage.login(driver, properties.getProperty(adminPropertyName));
+        adminHomePage.addTrainer().selectGroup(group);//TODO SELECT GROUP NEEDS FIXING IN AddTrainerPageImpl
+        Assertions.assertEquals(group, driver.findElement(By.id("addTrainerGroup")).getAttribute("value"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.close();
+    }
+
+    @AfterAll
+    static void teardownAll(){
+        driver.quit();
+    }
+}
