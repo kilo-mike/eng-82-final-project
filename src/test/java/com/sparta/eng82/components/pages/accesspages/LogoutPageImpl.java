8def1aca4b9320005397e9ef96b57374bd5679bd
Alex Sikorski <alex@alexsikorski.net>
2021-05-20 11:39:54 +0100

    login page interface refactor with default methods, new page object for logout page, more login tests for each type of user

--- src/test/java/com/sparta/eng82/components/pages/accesspages/LogoutPageImpl.java
+++ src/test/java/com/sparta/eng82/components/pages/accesspages/LogoutPageImpl.java
@@ -3,12 +3,15 @@ package com.sparta.eng82.components.pages.accesspages;
 import com.sparta.eng82.interfaces.pages.accesspages.LoginPage;
 import org.openqa.selenium.WebDriver;
 
-public class LoginPageImpl implements LoginPage {
+public class LogoutPageImpl implements LoginPage {
 
     private WebDriver driver;
 
-    public LoginPageImpl(WebDriver driver) {
+    public LogoutPageImpl(WebDriver driver) {
         this.driver = driver;
-        driver.get("http://localhost:8080/login");
+    }
+
+    public boolean isLogoutMessageShowing() {
+        return getUrl(driver).endsWith("?logout");
     }
 }
