275a5664d2c302d2f19ee2cb2091793ec9731bee
pkpatch <Kilmacno1@hotmail.com>
2021-05-25 13:20:26 +0100

    TraineeTrainer complete

--- src/test/java/com/sparta/eng82/components/pages/trainee/feedback/TraineeTrainerFeedbackFormPage.java
+++ src/test/java/com/sparta/eng82/components/pages/trainee/feedback/TraineeTrainerFeedbackFormPage.java
@@ -10,52 +10,27 @@ public interface TraineeTrainerFeedbackFormPage extends TraineeFeedbackFormPage 
 
     String getStopCommentBox();
 
-    TraineeTrainerFeedbackFormPage setStopCommentBox(String comments);
-
-    TraineeTrainerFeedbackFormPage editStopCommentBox(String comments);
-
-    TraineeTrainerFeedbackFormPage deleteStopCommentBox();
-
     boolean isStopCommentBoxEmpty();
 
     boolean isStopCommentBoxDisabled();
-
 
     TraineeTrainerFeedbackFormPage clickStartTab();
 
     String getStartCommentBox();
 
-    TraineeTrainerFeedbackFormPage setStartCommentBox(String comments);
-
-    TraineeTrainerFeedbackFormPage editStartCommentBox(String comments);
-
-    TraineeTrainerFeedbackFormPage deleteStartCommentBox();
-
     boolean isStartCommentBoxEmpty();
 
     boolean isStartCommentBoxDisabled();
-
 
     TraineeTrainerFeedbackFormPage clickContinueTab();
 
     String getContinueCommentBox();
 
-    TraineeTrainerFeedbackFormPage setContinueCommentBox(String comments);
-
-    TraineeTrainerFeedbackFormPage editContinueCommentBox(String comments);
-
-    TraineeTrainerFeedbackFormPage deleteContinueCommentBox();
-
     boolean isContinueCommentBoxEmpty();
 
     boolean isContinueCommentBoxDisabled();
 
-
     String getTrainerCommentBox();
-
-    TraineeTrainerFeedbackFormPage setTrainerCommentBox(String comments);
-
-    TraineeTrainerFeedbackFormPage deleteTrainerCommentBox();
 
     boolean isTrainerCommentBoxEmpty();
 
