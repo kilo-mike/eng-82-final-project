77f38e18354d4240548423ab60a740552ad1234a
abdulwm97 <abdulwm09@gmail.com>
2021-05-26 19:04:50 +0100

    A lot more reliable, but still doesn't pass 100% of the time

--- src/test/java/com/sparta/eng82/components/pages/admin/AddTrainerPageImpl.java
+++ src/test/java/com/sparta/eng82/components/pages/admin/AddTrainerPageImpl.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.support.ui.Select;
 import com.sparta.eng82.components.frameworkutil.ActionClicker;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 public class AddTrainerPageImpl implements AddTrainerPage {
 
     private final By addTrainerButton = new By.ByLinkText("Add Trainer");
@@ -22,12 +24,14 @@ public class AddTrainerPageImpl implements AddTrainerPage {
 
     @Override
     public AddTrainerPage enterFirstName(String firstName) {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.findElement(firstNameTextBox).sendKeys(firstName);
         return new AddTrainerPageImpl(driver, user);
     }
 
     @Override
     public AddTrainerPage enterSecondName(String lastName) {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.findElement(lastNameTextBox).sendKeys(lastName);
         return new AddTrainerPageImpl(driver, user);
     }
@@ -36,12 +40,14 @@ public class AddTrainerPageImpl implements AddTrainerPage {
     public AddTrainerPage selectGroup(String groupName) {
         new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(driver.findElement(new By.ById("addTrainerGroup"))));
         Select drpsGroup = new Select(driver.findElement(new By.ById("addTrainerGroup")));
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         drpsGroup.selectByVisibleText(groupName);
         return new AddTrainerPageImpl(driver, user);
     }
 
     @Override
     public AdminHomePage addNewTrainer() {
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 //        driver.findElement(addNewTrainerButton).click();
         ActionClicker.timedSingleMouseClicker(driver,ActionClicker.TIME,new By.ByXPath("//*[@id=\"trainerModal\"]/div/div/form/div[2]/button"));
         return new AdminHomePageImpl(driver, user);
